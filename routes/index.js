b653105c462705136972e085844a3ac0ac2586b1
mcoppola <mcoppola832@gmail.com>
2014-09-12 09:01:02 -0400

    started working on spreadsheet integration, working - need to make request not on page load

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,49 @@
+var Spreadsheet = require('edit-google-spreadsheet');
+var async = require('async');
+
 module.exports = function (app) {
     app.get('/', index);
 };
 
+var getSpreadsheetData = function(callback) {
+
+	Spreadsheet.load({
+	    debug: true,
+	    spreadsheetId: '174-MA3rEijKsXfU5sifHrZ59MqEpfjmcQ6_aIjK9G3s',
+	    worksheetId: 'od6',
+
+	    oauth : {
+	        email: '254154442815-4tk24uc0n66h8jn58pq9q1kgrnoc0itj@developer.gserviceaccount.com',
+	        keyFile: 'key-file.pem'
+	    }
+
+	}, function sheetReady(err, spreadsheet) {
+
+	    if (err) {
+	        callback(err);
+	    }
+
+	    spreadsheet.receive(function(err, rows, info) {
+	        if (err) {
+	            throw err;
+	        }
+
+	        // format the data
+	        var data = {};
+	        console.log(rows['2']['2']);
+	        data.monday = rows['2']['2'];
+
+	        callback(null, data);
+	    });
+
+	});
+	
+}
+
 var index = function (req, res) {
-    res.render('index', { title: 'MUSEY | LAND' });
+	// Lets get our hours and return it to the template
+	async.series([ getSpreadsheetData ], function(err, results) {
+			res.render('index', { data: results });
+	});
+    
 };
