71de0183ef94a6df65f74673267b44537c93ac78
mcoppola <mcoppola832@gmail.com>
2014-09-16 17:09:12 -0400

    added new relic monitoring service

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var express = require('express'),
+var require('newrelic'),
+	express = require('express'),
     http = require('http'),
     app = express(),
     opts = require(__dirname + '/config/opts.js');
